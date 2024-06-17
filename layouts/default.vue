554f7f373e1aec2abd63f8328adc790681c8d4ff
FalaSolucoes <161637754+FalaSolucoes@users.noreply.github.com>
2024-06-16 21:17:37 -0300

    Fix Logo.svg

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
     <main>
       <div class="flex items-center justify-center py-3 px-4 fixed top-0 left-0 w-full pointer-events-none z-10">
         <div class="rounded-full py-2 px-6 bg-white border border-zinc-200 shadow-sm flex items-center gap-2 pointer-events-auto">
-          <img src="/logo.svg" height="20" class="h-[20px] w-auto pr-4" />
+          <img src="../public/logo.svg" height="20" class="h-[20px] w-auto pr-4" />
           <nuxt-link to="/" active-class="[&>*]:font-bold">
             <Button variant="ghost" size="sm">
               Início
